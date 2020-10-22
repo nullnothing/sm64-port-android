a12c995107772836282a12fab81888d75aec0175
nulln <47261624+nullnothing@users.noreply.github.com>
2020-10-23 02:17:12 +1000

    It's Funny Time.

--- src/pc/gfx/gfx_opengl.c
+++ src/pc/gfx/gfx_opengl.c
@@ -457,7 +457,7 @@ static void gfx_opengl_set_zmode_decal(bool zmode_decal) {
 }
 
 static void gfx_opengl_set_viewport(int x, int y, int width, int height) {
-    glViewport(x, y, width, height);
+    glViewport(0, 0, width, height);
     current_height = height;
 }
 
